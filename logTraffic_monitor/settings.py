eebefbc658da78a0bb05f654d0d7ea0b8724c877
0xk4n3ki <k4n3ki.ctf@gmail.com>
2025-09-07 21:13:01 +0530

    correcting mistake

--- logTraffic_monitor/settings.py
+++ logTraffic_monitor/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6ym)&wby$(i#jkodiwh8ct4)f-h=(%%_ds6q7-ii69skfh2b^('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
